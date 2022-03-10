95888c47eb3d3cc73db085e9e74da326233f94cb
Rivean <91164637+Rivean@users.noreply.github.com>
2022-03-09 20:48:38 -0600

    Update HospitalChargesProject.sql

--- HospitalChargesProject.sql
+++ HospitalChargesProject.sql
@@ -7,13 +7,23 @@ SELECT DRG_Definition, Provider_Name, Provider_State, Provider_id, Average_total
 FROM HospitalCharges
 ORDER BY Provider_State, Provider_Name, Average_Total_Payments DESC;
 
+--Find the average total payments for all DRGs for a specific provider
+DECLARE @ProviderId int;
+
+SET @ProviderId = 50441;
+
+SELECT DRG_Definition, Provider_Name, Average_total_payments
+FROM HospitalCharges
+WHERE Provider_id = @ProviderId;
+
 
 --View how much hospitals in a state charge on average for a specific Diagnosis Related Group (DRG)
-DECLARE @Code as varchar(max)
-DECLARE @State as nchar(2)
+DECLARE @Code as varchar(max);
+DECLARE @State as nchar(2);
+
 --For @Code can change the number but must leave the "%" symbol there
-SET @Code = '207%'
-SET @State = 'TX'
+SET @Code = '207%';
+SET @State = 'TX';
 
 SELECT DRG_Definition, Provider_Name, Provider_State, Provider_id, Average_total_payments, Average_Medicare_Payments
 FROM HospitalCharges
@@ -38,15 +48,16 @@ ORDER BY Average_Total_Payments DESC
 RETURN
 END;
 
+
 SELECT *
 FROM dbo.DrgStateCost('192%', 'TX')
 ORDER BY AvgPayment DESC;
 
 
 -- Which hospitals charge the most for the same DRG?
-DECLARE @DRGCode as varchar(max)
+DECLARE @DRGCode as varchar(max);
 
-SET @DRGCode = '207%'
+SET @DRGCode = '207%';
 
 SELECT DRG_Definition, Provider_Name, Provider_State, Provider_id, Average_total_payments
 FROM HospitalCharges
@@ -54,19 +65,18 @@ WHERE DRG_Definition like @DRGCode
 ORDER BY Average_Total_Payments DESC;
 
 
--- What is the avg cost for each DRG, and which hospitals are above or below avg cost?
-DECLARE @DRGCode as varchar(max)
+-- What is the avg cost for a specific DRG, and does the hospital charge above or below the AVG for that DRG across the country? Ordered by state with below average costs coming first
+DECLARE @DRGCode as varchar(max);
 
-SET @DRGCode = '207%'
+SET @DRGCode = '207%';
 
-SELECT DRG_Definition, Provider_Name, Provider_State, Provider_id, Average_total_payments, Average_Medicare_Payments,
+SELECT DRG_Definition, Provider_Name, Provider_State, Provider_id, Average_total_payments, Average_Medicare_Payments, AVG(Average_total_payments) over(PARTITION BY DRG_Definition) as Average_Cost,
 CASE
 	WHEN Average_total_payments > AVG(Average_total_payments) over(PARTITION BY DRG_Definition) THEN 'Above Average'
 	WHEN Average_total_payments < AVG(Average_total_payments) over(PARTITION BY DRG_Definition) THEN 'Below Average'
 	WHEN Average_total_payments = AVG(Average_total_payments) over(PARTITION BY DRG_Definition) THEN 'Average'
-	END AS DRGCost
+	END AS Hospital_DRG_Cost
 FROM HospitalCharges
 GROUP BY DRG_Definition, Provider_Name, Provider_State, Provider_id, Average_total_payments, Average_Medicare_Payments
 HAVING DRG_Definition like @DRGCode
-ORDER BY Provider_State;
-
+ORDER BY Provider_State, Hospital_DRG_Cost DESC;
